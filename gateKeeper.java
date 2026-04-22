3bcc4a3b9cca1ca12e7e637d7462cf20ad86697c
Meetr <meetrandhawa2885@gmail.com>
2026-04-22 13:25:47 +0530

    synchronized

--- gateKeeper.java
+++ gateKeeper.java
@@ -1292,4 +1292,33 @@ non-static, inside method, container or any block
 //     }
 // }
 
+//! synchronized keyword
 
+public class gateKeeper{
+    public static void main(String[] args) throws Exception{
+        Counter c = new Counter();
+        Thread t1 = new Thread(()->{
+            for(int i=0; i<1000; i++){
+                c.increament();
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            for(int i=0; i<1000; i++){
+                c.increament();
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count: " + c.count);
+    }
+}
+
+class Counter{
+    int count = 0;
+    synchronized void increament(){
+        count++;
+    }
+}
